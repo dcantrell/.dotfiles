49e52d634c2ce9ebc1c4ea6f0c642ebacb244462
David Cantrell <dcantrell@burdell.org>
2024-01-23 13:08:50 -0500

    User preferences for Thunderbird

---
+++ .thunderbird/dcantrell/user.js
@@ -0,0 +1,6 @@
+user_pref("mailnews.send_plaintext_flowed", false);
+user_pref("mailnews.display.disable_format_flowed_support", true);
+user_pref("mail.quoted_graphical", false);
+user_pref("mail.wrap_long_lines", false);
+user_pref("mailnews.wraplength", 0);
+user_pref("plain_text.wrap_long_lines", false);
